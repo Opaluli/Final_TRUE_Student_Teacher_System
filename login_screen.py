b68bd5e02da4b9ae9690bb413527eef30f006a6f
saarf <fredjsaar@gmail.com>
2025-04-08 12:40:54 +0300

    base code with bug on teachers side

--- login_screen.py
+++ login_screen.py
@@ -3,7 +3,6 @@ from base_screen import BaseScreen
 from text_input import TextInput
 from data import get_user_by_credentials
 
-
 class LoginScreen(BaseScreen):
     def __init__(self, screen):
         super().__init__(screen)
@@ -31,7 +30,7 @@ class LoginScreen(BaseScreen):
             if self.login_button.collidepoint(event.pos):
                 return self.try_login()
 
-    def draw8(self, screen):
+    def draw(self, screen):
         super().draw()
         self.username_input.draw(self.screen)
         self.password_input.draw(self.screen)
